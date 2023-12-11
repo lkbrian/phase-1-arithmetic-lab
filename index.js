1f4911aacf2fdea0c6a50075c0594776aa1438b9
lkbrian <lkbrian742@gmail.com>
2023-12-11 18:41:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+console.log(multiply);
+
+const random = (Math.floor(Math.random() *20))
+console.log(Math.floor(random))
+
+let mod ;
+let num3 =49;
+let num4 = 5
+mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(1,10,20,);
+console.log(max);
